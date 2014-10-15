6b471375eae3961cdb13feb228ba279d71482479
Sally Jenkinson <sally.jenkinson@gmail.com>
2014-10-15 14:39:01 +0100

    Started customising front-end styles

---
+++ app/assets/javascripts/sv-scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	//Navigation actions
+    $('body').addClass('js');
+    var $menu = $('#menu'),
+    $menulink = $('.menu-link');
+  
+	$menulink.click(function() {
+		$menulink.toggleClass('active');
+		$menu.toggleClass('active');
+		return false;
+	});
+
+});

62000455bae9a494c1097650fb1436ba94915cea
Sally Jenkinson <sally.jenkinson@gmail.com>
2014-11-18 11:13:45 +0000

    Added status validations and tests, some work on styles

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -23,11 +23,18 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do
+    post :create, status: {content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:testsally)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
     end
-
     assert_redirected_to status_path(assigns(:status))
   end
 
@@ -37,11 +44,13 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:testsally)
     get :edit, id: @status
     assert_response :success
   end
 
   test "should update status" do
+    sign_in users(:testsally)
     patch :update, id: @status, status: { content: @status.content}
     assert_redirected_to status_path(assigns(:status))
   end

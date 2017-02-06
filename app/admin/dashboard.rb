d960b09ad3ac882993ee03e72b6d6fd88399317f
Sally Jenkinson <sally.jenkinson@gmail.com>
2017-02-06 16:57:50 +0000

    Surfaced category names

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,15 +3,7 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    # div class: "blank_slate_container", id: "dashboard_default_message" do
-    #   span class: "blank_slate" do
-    #     span I18n.t("active_admin.dashboard_welcome.welcome")
-    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
-    #   end
-    # end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
     columns do
         column do
         
@@ -28,6 +20,7 @@ ActiveAdmin.register_page "Dashboard" do
                     column (:id) {|status| link_to(status.id, admin_status_path(status)) }
                     column (:profile_name) {|status| status.user.profile_name }
                     column :content
+                    column :category_id
                     column :created_at
                 end # table
             end # panel
